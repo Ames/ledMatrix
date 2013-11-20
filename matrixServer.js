fa4195383b508d8a41f0485c44d58914477ef248
Ames Bielenberg <amesbielenberg@gmail.com>
2013-11-20 16:11:17 -0500

    heroku doesn't really support websockets :(

--- matrixServer.js
+++ matrixServer.js
@@ -33,6 +33,15 @@ app.get('/main.css', function (req, res) {
 
 
 // socket server
+
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 io.sockets.on('connection', function (socket) {
 
 	console.log('connected');
